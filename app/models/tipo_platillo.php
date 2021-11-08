d0cd1fcec3162c035fee534bc86fa4afc4f38c9c
Rebeca <kattantoledo@gmail.com>
2021-11-08 13:05:53 -0600

    Modificacion de modelos

--- app/models/tipo_platillo.php
+++ app/models/tipo_platillo.php
@@ -1,7 +1,7 @@
 <?php
     $conexion=mysqli_connect('localhost:3308','root','','bd_restaurante');
 
-    $nom_tipo_producto =$_POST['nom_tipo_platillo'];
+    $nom_tipo_platillo =$_POST['nom_tipo_platillo'];
 
     $sql = "INSERT INTO `tipo_platillo` (`nom_tipo_platillo`) VALUES ('$nom_tipo_platillo')";
 
