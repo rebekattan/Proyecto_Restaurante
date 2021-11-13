5cd38334e82a0f6e331516b680abeadfbe6f4d0d
Rebeca <kattantoledo@gmail.com>
2021-11-12 19:26:29 -0600

    comision_detalle

--- app/controllers/comision_detalle.js
+++ app/controllers/comision_detalle.js
@@ -1,4 +1,46 @@
 $(document).ready(function(){
+    let $empleado = document.querySelector('#empleado');
+    let $comision = document.querySelector('#comision');
+    
+    function cargarEmpleado(){
+        $.ajax({
+            type: "GET",
+            url: "app/models/mostrarEmpleado.php",
+            success:function(response){
+                const empleados = JSON.parse(response)
+                
+                let template = '<option class="form-control" selected disabled>Seleccione una opción</option>'
+                
+                empleados.forEach(empleado => {
+                    template += `<option value="${empleado.cod_empleado}">${empleado.nombres}</option>`
+                })
+    
+                $empleado.innerHTML = template;
+            }
+        });
+    }
+
+    function cargarComision(){
+        $.ajax({
+            type: "GET",
+            url: "app/models/mostrarComision.php",
+            success:function(response){
+                const comisions = JSON.parse(response)
+                
+                let template = '<option class="form-control" selected disabled>Seleccione una opción</option>'
+                
+                comisions.forEach(comision => {
+                    template += `<option value="${comision.cod_comision}">${comision.comision}</option>`
+                })
+    
+                $comision.innerHTML = template;
+            }
+        });
+    }
+    
+    cargarEmpleado();
+    cargarComision();
+    
     $('#btn_frm_comision_detalle').click(function(){
         console.log("Entro a la funcion");
         var datos = $('#frm_comision_detalle').serialize();
